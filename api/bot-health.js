657a5ed218eb5036c35e5a6c4c05e2a07a2e544a
lucas354803 <lucaas3548@gmail.com>
2026-05-03 03:36:01 -0300

    Delete api directory

--- api/bot-health.js
+++
@@ -1,9 +0,0 @@
-import { safeEnvStatus } from '../lib/_supabase.js';
-
-export default async function handler(req, res) {
-  res.status(200).json({
-    ok: true,
-    message: 'API do robô Big Burger online',
-    env: safeEnvStatus()
-  });
-}
