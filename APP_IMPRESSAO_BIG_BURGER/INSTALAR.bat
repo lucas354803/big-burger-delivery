d83f41147b6c79f712afe1d636278a57089becb4
lucas354803 <lucaas3548@gmail.com>
2026-05-11 16:51:37 -0300

    Delete APP_IMPRESSAO_BIG_BURGER directory

--- APP_IMPRESSAO_BIG_BURGER/INSTALAR.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo Instalando APP Big Burger Impressao...
-npm install
-pause
--- APP_IMPRESSAO_BIG_BURGER/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('BigBurgerApp', {
-  printReceipt: (html) => ipcRenderer.invoke('bigburger-print-receipt', html),
-  printers: () => ipcRenderer.invoke('bigburger-list-printers')
-});
