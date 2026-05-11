d91e00fdcbd2222ddec30b7a825822d6fb615da3
lucas354803 <lucaas3548@gmail.com>
2026-05-10 22:42:57 -0300

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -11,7 +11,6 @@ import webhook from '../lib/api/webhook.js';
 import siteBanner from '../lib/api/site-banner.js';
 import zerarRelatorio from '../lib/api/zerar-relatorio.js';
 import historico from '../lib/api/historico.js';
-import testWhatsapp from '../lib/api/test-whatsapp.js';
 
 const routes = {
   'admin-menu': adminMenu,
@@ -27,7 +26,6 @@ const routes = {
   'site-banner': siteBanner,
   'zerar-relatorio': zerarRelatorio,
   historico,
-  'test-whatsapp': testWhatsapp,
 };
 
 export default async function handler(req, res) {
